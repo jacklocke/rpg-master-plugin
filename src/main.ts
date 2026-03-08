b0ff256c8c19652aa75c2cc0cbe5c554422ba2b0
Taggiasco <ashtj14@gmail.com>
2026-03-08 21:32:32 +0100

    dev area

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,55 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Plugin, Notice } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class RPGMasterPlugin extends Plugin {
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		// dichiaro le variabili della classe
+		statusBarTextElement: HTMLSpanElement;
 
-	async onload() {
-		await this.loadSettings();
+		async onload() {
+			console.log("RPG Master Plugin loaded");
+		
+			// Aggiunge alla sidebar una icona cliccabile
+			this.addRibbonIcon('dice', 'Greet', () => {
+				new Notice('Hello, world!');
+			});
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// Aggiunge un elemento alla status bar
+		this.statusBarTextElement = this.addStatusBarItem().createEl('span');
+		this.statusBarTextElement.setText('RPG Master Plugin is active');
+
+		// chiamo all'onload la funzione per leggere il file attivo e aggiornare il contatore di righe
+		this.leggiFileAttivoEAggiornaConteggioRighe();
+
+		// Aggiorna il contatore di righe quando il file attivo cambia
+		this.app.workspace.on('active-leaf-change', async () => {
+			this.leggiFileAttivoEAggiornaConteggioRighe();
 		});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+		// Aggiorna il contatore ad ogni cambio di contenuto del file attivo
+		this.app.workspace.on('editor-change', editor => {
+			const file = editor.getDoc().getValue();
+			this.aggiornaConteggioRighe(file);
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 
 	}
 
 	onunload() {
+		console.log("RPG Master Plugin unloaded");
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+	private async leggiFileAttivoEAggiornaConteggioRighe() {
+		const file = this.app.workspace.getActiveFile();
+		if (file) {
+			const content = await this.app.vault.read(file);
+			//console.log(`Contenuto del file attivo: ${content}`);
+			this.aggiornaConteggioRighe(content);
+		} else {
+			this.aggiornaConteggioRighe();
+		}
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	private aggiornaConteggioRighe(contenutoFile?: string) {
+		const conta = contenutoFile ? contenutoFile.split('\n').length : 0;
+		this.statusBarTextElement.setText(`Righe: ${conta}`);
 	}
 }
